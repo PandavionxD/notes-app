9f2fcb614bccca489f032ec3bdda7f12f7aa5b54
Alex Daniel <1151372@senati.pe>
2023-07-23 17:17:50 -0500

    Agregando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import '@fontsource/raleway/300.css';
-import '@fontsource/raleway/400.css';
-import '@fontsource/raleway/500.css';
-import '@fontsource/raleway/700.css';
+import "@fontsource/raleway/300.css";
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/500.css";
+import "@fontsource/raleway/700.css";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import { NotasPage } from "./pages/NotasPage";
+import { EditNotasPage } from "./pages/EditNotasPage";
+import { AddNotasPage } from "./pages/AddNotasPage";
 
 export const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<NotasPage />} />
+        <Route path="/edit/:id" element={<EditNotasPage />} />
+        <Route path="/add" element={<AddNotasPage />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
